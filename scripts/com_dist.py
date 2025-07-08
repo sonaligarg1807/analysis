912e6723d93257349f2ec2dc8cdd1720148e5ebf
Sonali Garg <sgarg@debian-BULLSEYE-live-builder-AMD64>
2025-07-08 16:09:29 +0200

    added xyz file reading in com distance calculation

--- scripts/com_dist.py
+++ scripts/com_dist.py
@@ -1,7 +1,8 @@
+#functionality of reading both .gro and xyz files
+
 import numpy as np
 
 class COMDistanceCalculator:
-    # Atomic masses of pentacene atoms (C = 12.011, H = 1.008)
     atom_masses = {
         'C': 12.011,
         'H': 1.008
@@ -9,37 +10,69 @@ class COMDistanceCalculator:
 
     nm_to_angstrom = 10.0
 
-    def __init__(self, gro_file, resid1, resid2, output_filename):
-        self.gro_file = gro_file
-        self.resid1 = resid1
-        self.resid2 = resid2
-        self.output_filename = output_filename
-        self.atoms = self.parse_gro_file()
+    def __init__(self, input_file, mode, mol1, mol2):
+        self.input_file = input_file
+        self.mode = mode
+        self.mol1 = mol1
+        self.mol2 = mol2
+
+        if mode == 'gro':
+            self.atoms = self.parse_gro_file()
+        elif mode == 'xyz':
+            self.atoms = self.parse_xyz_file()
+        else:
+            raise ValueError("Mode must be 'gro' or 'xyz'")
 
     def parse_gro_file(self):
         atoms = []
-        with open(self.gro_file, 'r') as file:
-            lines = file.readlines()[2:-1]  # Skip header and box line
+        with open(self.input_file, 'r') as file:
+            lines = file.readlines()[2:-1]
             for line in lines:
                 resid = int(line[0:5].strip())
                 atomname = line[10:15].strip()
                 x = float(line[20:28].strip())
                 y = float(line[28:36].strip())
                 z = float(line[36:44].strip())
-                atoms.append((resid, atomname, np.array([x, y, z])))
+                atoms.append((resid, atomname, np.array([x, y, z])))  # in nm
         return atoms
 
-    def get_molecule_atoms(self, resid):
-        return [atom for atom in self.atoms if atom[0] == resid]
+    def parse_xyz_file(self):
+        atoms = []
+        with open(self.input_file, 'r') as file:
+            lines = file.readlines()[2:]
+            for line in lines:
+                parts = line.strip().split()
+                if len(parts) < 4:
+                    continue
+                atomname = parts[0]
+                x, y, z = map(float, parts[1:4])
+                atoms.append((atomname, np.array([x, y, z])))  # in Å
+        return atoms
+
+    def get_molecule_atoms(self, identifier):
+        if self.mode == 'gro':
+            resid = identifier
+            return [atom for atom in self.atoms if atom[0] == resid]
+        elif self.mode == 'xyz':
+            start, end = identifier
+            return self.atoms[start - 3 : end - 2]  # adjust for header
+        else:
+            raise ValueError("Invalid mode")
 
     def calculate_com(self, atoms):
         total_mass = 0.0
         weighted_positions = np.zeros(3)
 
         for atom in atoms:
-            atomname = atom[1][0]  # First character: 'C' or 'H'
-            mass = self.atom_masses.get(atomname, 0)
-            weighted_positions += mass * atom[2]
+            if self.mode == 'gro':
+                atomname = atom[1][0]
+                pos = atom[2]
+            else:
+                atomname = atom[0][0]
+                pos = atom[1]
+
+            mass = self.atom_masses.get(atomname, 12.011)
+            weighted_positions += mass * pos
             total_mass += mass
 
         return weighted_positions / total_mass
@@ -47,27 +80,20 @@ class COMDistanceCalculator:
     def calculate_distance(self, com1, com2):
         return np.linalg.norm(com1 - com2)
 
-    def save_to_file(self, com1, com2, dist_nm, dist_angstrom):
-        with open(self.output_filename, 'w') as f:
-            f.write(f"Center of Mass of Molecule {self.resid1}: {com1}\n")
-            f.write(f"Center of Mass of Molecule {self.resid2}: {com2}\n")
-            f.write(f"Distance between COMs (nm): {dist_nm:.3f} nm\n")
-            f.write(f"Distance between COMs (angstroms): {dist_angstrom:.3f} Å\n")
-        print(f"Results saved to {self.output_filename}")
-
     def run(self):
-        mol1_atoms = self.get_molecule_atoms(self.resid1)
-        mol2_atoms = self.get_molecule_atoms(self.resid2)
+        mol1_atoms = self.get_molecule_atoms(self.mol1)
+        mol2_atoms = self.get_molecule_atoms(self.mol2)
 
         com1 = self.calculate_com(mol1_atoms)
         com2 = self.calculate_com(mol2_atoms)
+        dist_angstrom = self.calculate_distance(com1, com2)
 
-        dist_nm = self.calculate_distance(com1, com2)
-        dist_angstrom = dist_nm * self.nm_to_angstrom
+        if self.mode == 'gro':
+            dist_nm = dist_angstrom
+            dist_angstrom *= self.nm_to_angstrom
+        else:
+            dist_nm = dist_angstrom / self.nm_to_angstrom
 
-        print(f"Center of Mass of Molecule {self.resid1}: {com1}")
-        print(f"Center of Mass of Molecule {self.resid2}: {com2}")
-        print(f"Distance between COMs: {dist_nm:.3f} nm")
-        print(f"Distance between COMs: {dist_angstrom:.3f} Å")
-
-        self.save_to_file(com1, com2, dist_nm, dist_angstrom)
+        print(f"COM Molecule 1: {com1}")
+        print(f"COM Molecule 2: {com2}")
+        print(f"Distance between COMs: {dist_nm:.3f} nm | {dist_angstrom:.3f} Å")
